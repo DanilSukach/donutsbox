7e7d7eb7276e86bf0f242cf18a7a736b8d091775
DanilSukach <danil211-1@yandex.ru>
2025-09-11 18:05:57 +0400

    Add folders

--- DonutsboxServer/Auth.Api/Program.cs
+++ DonutsboxServer/Auth.Api/Program.cs
@@ -1,6 +1,27 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 app.Run();
