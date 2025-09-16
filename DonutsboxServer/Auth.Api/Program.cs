1b6e080f9f2a972c3a8110ce543d624860487c64
DanilSukach <danil211-1@yandex.ru>
2025-09-16 13:56:21 +0400

    add connection strins

--- DonutsboxServer/Auth.Api/Program.cs
+++ DonutsboxServer/Auth.Api/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(xmlPath);
 });
 
+builder.Configuration
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+       .AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
