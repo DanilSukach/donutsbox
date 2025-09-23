6b9f8d96f05fc0e044b5109d8baf5a902349286c
Ivan K <buckingkiska@gmail.com>
2025-09-23 23:51:31 +0400

    Репозитории для сущностей

---
+++ DonutsboxServer/Donutsbox.Domain/Repositories/ContentPostRepository.cs
@@ -0,0 +1,60 @@
+﻿using Donutsbox.Domain.Context;
+using Donutsbox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Donutsbox.Domain.Repositories;
+
+public class ContentPostRepository(DonutsboxDbContext context) : IEntityRepository<ContentPost, string>
+{
+    public async Task<ContentPost> AddAsync(ContentPost entity)
+    {
+        var post = await context.ContentPosts.AddAsync(entity);
+        await context.SaveChangesAsync();
+        return post.Entity;
+    }
+
+    public async Task<bool> DeleteAsync(string id)
+    {
+        var post = await GetByIdAsync(id);
+        if (post == null) return false;
+        context.ContentPosts.Remove(post);
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<IEnumerable<ContentPost>> GetAllAsync()
+    {
+        return await context.ContentPosts.ToListAsync();
+    }
+
+    public async Task<ContentPost?> GetByIdAsync(string id)
+    {
+        return await context.ContentPosts
+                            .FirstOrDefaultAsync(c => c.PostId == id);
+    }
+
+    public async Task<bool> UpdateAsync(ContentPost entity, string id)
+    {
+        var post = await GetByIdAsync(id);
+        if (post == null) return false;
+        post.Title = entity.Title;
+        post.Text = entity.Text;
+        post.AudioURLs = entity.AudioURLs;
+        post.PictureURLs = entity.PictureURLs;
+        post.VideoURLs = entity.VideoURLs;
+        post.DislikesCount = entity.DislikesCount;
+        post.CreatedAt = entity.CreatedAt;
+        post.PageId = entity.PageId;
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> UpdateDislikesAsync(string id, int dislikesCount)
+    {
+        var post = await GetByIdAsync(id);
+        if (post == null) return false;
+        post.DislikesCount = dislikesCount;
+        await context.SaveChangesAsync();
+        return true;
+    }
+}
---
+++ DonutsboxServer/Donutsbox.Domain/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,48 @@
+﻿using Donutsbox.Domain.Context;
+using Donutsbox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Donutsbox.Domain.Repositories;
+
+public class SubscriptionRepository(DonutsboxDbContext context) : IEntityRepository<Subscription, string>
+{
+    public async Task<Subscription> AddAsync(Subscription entity)
+    {
+        var subscription = await context.Subscriptions.AddAsync(entity);
+        await context.SaveChangesAsync();
+        return subscription.Entity;
+    }
+
+    public async Task<bool> DeleteAsync(string id)
+    {
+        var subscription = await GetByIdAsync(id);
+        if (subscription == null) return false;
+        context.Subscriptions.Remove(subscription);
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<IEnumerable<Subscription>> GetAllAsync()
+    {
+        return await context.Subscriptions.ToListAsync();
+    }
+
+    public async Task<Subscription?> GetByIdAsync(string id)
+    {
+        return await context.Subscriptions
+                            .FirstOrDefaultAsync(s => s.SubscriptionId == id);
+    }
+
+    public async Task<bool> UpdateAsync(Subscription entity, string id)
+    {
+        var subscription = await GetByIdAsync(id);
+        if (subscription == null) return false;
+        subscription.Name = entity.Name;
+        subscription.PageId = entity.PageId;
+        subscription.Price = entity.Price;
+        subscription.Description = entity.Description;
+        subscription.PictureURL = entity.PictureURL;
+        await context.SaveChangesAsync();
+        return true;
+    }
+}
