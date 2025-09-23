79bbb6aff0ca0ca6c036778b37a6164c36793311
Andrei <marinin1337@gmail.com>
2025-09-23 23:52:27 +0400

    Создал Репозитории

---
+++ DonutsboxServer/Donutsbox.Domain/Repositories/UserDataRepository.cs
@@ -0,0 +1,48 @@
+﻿using Donutsbox.Domain.Context;
+using Donutsbox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Donutsbox.Domain.Repositories;
+public class UserDataRepository(DonutsboxDbContext context) : IEntityRepository<UserData, string>
+{
+    public async Task<UserData> AddAsync(UserData entity)
+    {
+        context.UsersData.Add(entity);
+        await context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(string id)
+    {
+        var oldValue = await GetByIdAsync(id);
+        if (oldValue == null)
+        {
+            return false;
+        }
+        context.UsersData.Remove(oldValue);
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<IEnumerable<UserData>> GetAllAsync() => await context.UsersData.ToListAsync();
+
+    public async Task<UserData?> GetByIdAsync(string id) => await context.UsersData.FirstOrDefaultAsync(ud => ud.GUID == id);
+
+    public async Task<bool> UpdateAsync(UserData entity, string id)
+    {
+        var oldValue = await GetByIdAsync(id);
+        if (oldValue == null)
+        {
+            return false;
+        }
+        oldValue.AvatarUrl = entity.AvatarUrl;
+        oldValue.Description = entity.Description;
+        oldValue.NotificationEmail = entity.NotificationEmail;
+        oldValue.PhoneNumber = entity.PhoneNumber;
+        oldValue.PaymentInfo = entity.PaymentInfo;
+        await context.SaveChangesAsync();
+        return true;
+    }
+}
+
+
