7354e432b26fdcde1042f16dfe652ada60755468
Ivan K <buckingkiska@gmail.com>
2025-09-24 00:31:29 +0400

    Интерфейс для сервисов

---
+++ DonutsboxServer/Donutsbox.Api/Services/IEntityService.cs
@@ -0,0 +1,13 @@
+﻿namespace Donutsbox.Api.Services;
+
+/// <summary>
+/// Интерфейс для сервисов
+/// </summary>
+public interface IEntityService<DTO, TId>
+{
+    public Task<IEnumerable<DTO>> GetAllAsync();
+    public Task<DTO?> GetById(TId id);
+    public Task<DTO?> Add(DTO entity);
+    public Task<bool> Update(DTO entity, TId id);
+    public Task<bool> Delete(TId id);
+}
