8c181cbe66ffaa39f4500d4a05c7c9f460c1b5fa
Ivan K <buckingkiska@gmail.com>
2025-09-23 21:03:58 +0400

    Сущность юзер, интерфейс репозитория

--- DonutsboxServer/Donutsbox.Domain/Entities/User.cs
+++ DonutsboxServer/Donutsbox.Domain/Entities/User.cs
@@ -1,5 +1,35 @@
-﻿namespace Donutsbox.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-class User
+namespace Donutsbox.Domain.Entities;
+
+/// <summary>
+/// Обычный пользователь
+/// </summary>
+[Table("Users")]
+public class User
 {
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    [Key]
+    public required string GUID { get; set; }
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    [Column("Name")]
+    [Required]
+    public required string Name { get; set; }
+    /// <summary>
+    /// Тип пользователя
+    /// </summary>
+    [Column("TypeID")]
+    [Required]
+    public required string TypeID { get; set; }
+    /// <summary>
+    /// Сущность для авторизации
+    /// </summary>
+    [Column("AuthID")]
+    [Required]
+    public required string AuthID { get; set; }
 }
---
+++ DonutsboxServer/Donutsbox.Domain/Repositories/IEntityRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Donutsbox.Domain.Repositories;
+
+public interface IEntityRepository<T,TId>
+{
+    public Task<IEnumerable<T>> GetAllAsync();
+    public Task<T?> GetByIdAsync(TId id);
+    public Task<T> AddAsync(T entity);
+    public Task<bool> UpdateAsync(T entity);
+    public Task<bool> DeleteAsync(TId id);
+}
