2cb3aec21897bb633f6f1b7c7dab0dc6ef7a8ae6
Andrei <marinin1337@gmail.com>
2025-09-23 23:52:27 +0400

    Создал Репозитории

---
+++ DonutsboxServer/Donutsbox.Domain/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Donutsbox.Domain.Context;
+using Donutsbox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Donutsbox.Domain.Repositories;
+
+public class UserRepository(DonutsboxDbContext context) : IEntityRepository<User, string>
+{
+    public async Task<User> AddAsync(User entity)
+    {
+        context.Users.Add(entity);
+        await context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(string id)
+    {
+        var oldValue = await GetByIdAsync(id);
+        if (oldValue == null)
+        {
+            return false;
+        }
+        context.Users.Remove(oldValue);
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync() => await context.Users.ToListAsync();
+
+    public async Task<User?> GetByIdAsync(string id) => await context.Users.FirstOrDefaultAsync(u => u.GUID == id);
+
+    public async Task<bool> UpdateAsync(User entity, string id)
+    {
+        var oldValue = await GetByIdAsync(id);
+        if (oldValue == null)
+        {
+            return false;
+        }
+        oldValue.Name = entity.Name;
+        oldValue.TypeId = entity.TypeId;
+        oldValue.AuthId = entity.AuthId;
+        await context.SaveChangesAsync();
+        return true;
+    }
+}
+
